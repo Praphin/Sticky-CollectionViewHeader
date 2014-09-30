acd476fe7795999a83eb1fff5737bded0f6e3aed
Praphin SP <sppraphin@gmail.com>
2014-09-30 18:53:12 +0530

    Initial Commit

---
+++ StckyUICollectionHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StckyUICollectionHeader
+//
+//  Created by Praphin SP on 30/09/14.
+//  Copyright (c) 2014 Praphin SP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StckyUICollectionHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StckyUICollectionHeader
+//
+//  Created by Praphin SP on 30/09/14.
+//  Copyright (c) 2014 Praphin SP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
